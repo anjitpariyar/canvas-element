3f25d020b5fb515ae58bb503e911c041df836425
anjitzero <45827003+anjitzero@users.noreply.github.com>
2020-02-17 09:41:38 +0545

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,53 @@
+PIXI.utils.sayHello();
+var pixEle = document.getElementById('display');
+
+var renderer = PIXI.autoDetectRenderer(512,512, {
+	transparent:true,
+	resolution:1,
+});
+pixEle.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+PIXI.loader
+.add("parasite", './parasite.jpeg')
+.load(setup)
+
+var parasite;
+function setup(){
+	stage.interactive = true;
+	var cWidth = document.querySelector('canvas').offsetWidth;
+	var cHeight = document.querySelector('canvas').offsetHeight;
+	parasite = new PIXI.Sprite(
+		PIXI.loader.resources['parasite'].texture
+		);
+	parasite.height = cHeight;
+	parasite.width = cWidth;
+	animationLoop(cHeight, cWidth);
+
+	parasite.interactive = true;
+
+	parasite.click = function(){
+		this.scale.x += 0.06;
+		this.scale.y += 0.06;
+		this.pivot.set(200, 200)
+
+	}
+
+	stage.addChild(parasite);
+
+}
+
+function animationLoop(cHeight, cWidth){
+	requestAnimationFrame(animationLoop);
+	parasite.rotation  += 0.001;
+	// parasite.x= cWidth/2;
+	// parasite.y= cHeight/2;
+
+
+	// parasite.anchor.set(0.5,0.5)
+	// parasite.pivot.set(200, 200)
+
+	renderer.render(stage);
+
+}
+
